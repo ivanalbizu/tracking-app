4cb840f7fe6a3924fcbbe9734478d9b62cd1c805
ivanalbizu <ivan.albizu@gmail.com>
2020-08-16 18:44:23 +0200

    Heroku

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ app.use('/api', require('./routes/index'));
 if (process.env.NODE_ENV === 'production') {
   // Static folder
   app.use(express.static(__dirname + '/public/'));
-  
   // Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
